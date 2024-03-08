1371255cd57ced38df7c1944acae288d22dcc936
Applejuice7 <162735937+Applejuice7@users.noreply.github.com>
2024-03-08 23:04:06 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,94 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        `, mySprite, 0, -50)
+    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    sprites.destroy(sprite, effects.disintegrate, 500)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.fire, 500)
+    scene.cameraShake(4, 500)
+    info.changeLifeBy(-1)
+})
+let burger: Sprite = null
+let projectile: Sprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 3 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 3 . . . . . . . 
+    . . . . . . 8 8 1 a . . . . . . 
+    . . . . . . 8 3 1 a . . . . . . 
+    . . . . . c c c a a a . . . . . 
+    . . . . 8 8 3 3 3 1 a a . . . . 
+    . . 8 f f f c c a a f f a a . . 
+    . 8 8 8 8 a a 3 3 3 3 1 3 a a . 
+    8 8 8 8 8 8 a a 3 3 3 1 3 3 a a 
+    8 8 8 8 8 8 a a 3 3 3 3 1 3 a a 
+    `, SpriteKind.Player)
+mySprite.setPosition(78, 109)
+controller.moveSprite(mySprite, 100, 0)
+mySprite.setStayInScreen(true)
+game.onUpdateInterval(1000, function () {
+    burger = sprites.createProjectileFromSide(img`
+        ...........ccccc66666...........
+        ........ccc4444444444666........
+        ......cc444444444bb4444466......
+        .....cb4444bb4444b5b444444b.....
+        ....eb4444b5b44444b44444444b....
+        ...ebb44444b4444444444b444446...
+        ..eb6bb444444444bb444b5b444446..
+        ..e6bb5b44444444b5b444b44bb44e..
+        .e66b4b4444444444b4444444b5b44e.
+        .e6bb444444444444444444444bb44e.
+        eb66b44444bb444444444444444444be
+        eb66bb444b5b44444444bb44444444be
+        fb666b444bb444444444b5b4444444bf
+        fcb666b44444444444444bb444444bcf
+        .fbb6666b44444444444444444444bf.
+        .efbb66666bb4444444444444444bfe.
+        .86fcbb66666bbb44444444444bcc688
+        8772effcbbbbbbbbbbbbbbbbcfc22778
+        87722222cccccccccccccccc22226678
+        f866622222222222222222222276686f
+        fef866677766667777776667777fffef
+        fbff877768f86777777666776fffffbf
+        fbeffeefffeff7766688effeeeefeb6f
+        f6bfffeffeeeeeeeeeeeeefeeeeebb6e
+        f66ddfffffeeeffeffeeeeeffeedb46e
+        .c66ddd4effffffeeeeeffff4ddb46e.
+        .fc6b4dddddddddddddddddddb444ee.
+        ..ff6bb444444444444444444444ee..
+        ....ffbbbb4444444444444444ee....
+        ......ffebbbbbb44444444eee......
+        .........fffffffcccccee.........
+        ................................
+        `, 0, 50)
+    burger.x = randint(0, scene.screenWidth())
+    burger.setKind(SpriteKind.Enemy)
+})
